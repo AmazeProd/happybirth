d9c7d371113cbaf8b8a4c9d11db1e9a358f4b4b7
Naitik tiwari <106234126+AmazeProd@users.noreply.github.com>
2023-06-27 20:45:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function op() {
+  document.body.innerHTML = `<img class = "img" src="happy-birthday.gif" alt="">`
+  document.body.style.backgroundColor = "black";
+  document.body.innerHTML += `<h1 class="vir"><p class = virx>Robin</p><p class = "pvir">Best wishes For You</p><p class = "pvir2">From Your Best Friend Naitik</p></h1>`
+  
+  
+
+
+  // body...
+}
